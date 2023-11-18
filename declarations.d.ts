255454e59315e175e0624c50ccfa850655b0d2de
kierancrown <kieran.crown@gmail.com>
2023-11-18 16:04:09 +0000

    added env vars

--- declarations.d.ts
+++ declarations.d.ts
@@ -4,3 +4,9 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare module '@env' {
+  export const API_KEY: string;
+  export const API_HOST: string;
+  export const API_URL: string;
+}
